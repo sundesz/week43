3af2dc93f47fdda0450d5d3804adbe94d6cc0fec
Sandesh Hyoju <sundesz@gmail.com>
2014-10-23 14:16:01 +0300

    changes

--- index.php
+++ index.php
@@ -1,22 +1,48 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Week 43 Amazon task</title>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+		<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+		<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+	</head>
+	<body>
+
 <?php
+	if ( isset( $_POST['date'] )) {
 
-if ( isset( $_POST['date'] )) {
+		$date1 = date ( "Y-m-d", strtotime($_POST['date']) );
+		$date2 = date("Y-m-d");
 
-	$date1 = date ( "Y-m-d", strtotime($_POST['date']) );
-	$date2 = date("Y-m-d");
+		echo "Date you select: " , $date1, '<br>';
 
-	echo $date1, '<br>';
+		$diff = abs(strtotime($date2) - strtotime($date1));
+		$days = floor(($diff )/ (60*60*24));
 
-	$diff = abs(strtotime($date2) - strtotime($date1));
-	$days = floor(($diff )/ (60*60*24));
-
-	printf("%d days\n", $days);
-} else  {
+		printf("%d days\n", $days);
+	} else  {
 ?>
-<h3> Please enter date in month, day, year format </h3>
-<form method="POST" >
-<input type="text" name="date" placeholder="Enter a date"/>
-<input type="submit" name="submit" value="Submit">
-</form>
+<h3> Please select date </h3>
+	<form method="POST" >
+		<p>Date: <input type="text" name="date" id="datepicker" readonly>
+			<input type="submit" id="submit" name="submit" value="Submit">
+		</p>
+	</form>
 
 <?php } ?>
+
+	<script>
+		$(function() {
+			$( "#datepicker" ).datepicker();
+			
+			$("#submit").click(function() {
+				if ( $("#datepicker").val() == '') {
+					alert("Please select date first");
+					return false;
+				}
+			});
+		});
+	</script>
+	</body>
+</html>
